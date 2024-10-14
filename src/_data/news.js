2841337080b69aaf86107146eafd46468c551245
ayokunle ogunfidodo <89479717+fidodo@users.noreply.github.com>
2024-10-14 14:55:26 +0300

    displaying dynamic data

---
+++ src/_data/news.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+require('dotenv').config();
+
+module.exports = async function() {
+  try {
+const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=99f5205af4544a20bfdf4cbd5687bb21&pageSize=15`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+
+};
