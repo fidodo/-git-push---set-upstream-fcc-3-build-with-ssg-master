bed7e7e9401f6af673cbc3d0eea6ac00fa47dff8
ayokunle ogunfidodo <89479717+fidodo@users.noreply.github.com>
2024-10-15 10:22:45 +0300

    change country news

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = async function() {
   try {
-const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=99f5205af4544a20bfdf4cbd5687bb21&pageSize=15`);
+const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=ng&category=business&apiKey=99f5205af4544a20bfdf4cbd5687bb21&pageSize=15`);
     return response.data;
   } catch (error) {
     console.error(error);
